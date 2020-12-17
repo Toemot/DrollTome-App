dcf23eb4855e2e790d2c9c4c838f70f98d59d08b
Otokiti Ola <otokiti.tosin@gmail.com>
2020-12-17 19:59:43 +0200

    All Model Classes Added, Database Configured, Sample Data Test Used

---
+++ DrollTome-App/ComicBookGalleryModel/Models/Series.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class Series
+    {
+        public Series()
+        {
+            ComicBooks = new List<ComicBookArtist>();
+        }
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<ComicBookArtist> ComicBooks { get; set; }
+    }
+}
--- DrollTome-App/ComicBookGalleryModel/Program.cs
+++ DrollTome-App/ComicBookGalleryModel/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGalleryModel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace ComicBookGalleryModel
     {
         static void Main(string[] args)
         {
+            using (var context = new Context()) 
+            {
+                var series1 = new Series { Title = "The Amazing SpiderMan"};
+                context.ComicBooks.Add(new ComicBook
+                {
+                    Series = series1,
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+
+                var series2 = new Series { Title = "The Amazing SpiderMan2" };
+                context.ComicBooks.Add(new ComicBook
+                {
+                    Series = series2,
+                    IssueNumber = 2,
+                    PublishedOn = DateTime.Today
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+
+                foreach (ComicBook book in comicBooks)
+                {
+                    Console.WriteLine(book.DisplayText);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
